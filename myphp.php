b299e8d872257e79889ccdec8ce50371a05c2e9a
Jajoe2016 <joeyimer@yahoo.com>
2022-04-03 20:00:24 -0700

    Update myphp.php

--- myphp.php
+++ myphp.php
@@ -1,6 +1,4 @@
 <?php
-// echo "Hello World!";
-
 if (!file_exists('madeline.php')) {
     copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
@@ -8,21 +6,16 @@ include 'madeline.php';
 
 $MadelineProto = new \danog\MadelineProto\API('session.madeline');
 $MadelineProto->start();
-
 $me = $MadelineProto->getSelf();
 
 $MadelineProto->logger($me);
 
 if (!$me['bot']) {
-    $MadelineProto->messages->sendMessage(['peer' => '@sh_agent1', 'message' => "Hi!\nThis is message from bot"]);
-    // $MadelineProto->channels->joinChannel(['channel' => '@MadelineProto']);
-
+    $MadelineProto->messages->sendMessage(['peer' => '<TELEGRAM_USER>', 'message' => "Hi!\nThis is message from bot"]);
     try {
         $MadelineProto->messages->importChatInvite(['hash' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg']);
     } catch (\danog\MadelineProto\RPCErrorException $e) {
         $MadelineProto->logger($e);
     }
-
-    // $MadelineProto->messages->sendMessage(['peer' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg', 'message' => 'Testing MadelineProto!']);
 }
 $MadelineProto->echo('OK, done!');

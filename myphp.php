a138236da74600e073450f42063a83f266bf1671
Jajoe2016 <joeyimer@yahoo.com>
2022-04-03 19:56:13 -0700

    Add files via upload

---
+++ myphp.php
@@ -0,0 +1,28 @@
+<?php
+// echo "Hello World!";
+
+if (!file_exists('madeline.php')) {
+    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
+}
+include 'madeline.php';
+
+$MadelineProto = new \danog\MadelineProto\API('session.madeline');
+$MadelineProto->start();
+
+$me = $MadelineProto->getSelf();
+
+$MadelineProto->logger($me);
+
+if (!$me['bot']) {
+    $MadelineProto->messages->sendMessage(['peer' => '@sh_agent1', 'message' => "Hi!\nThis is message from bot"]);
+    // $MadelineProto->channels->joinChannel(['channel' => '@MadelineProto']);
+
+    try {
+        $MadelineProto->messages->importChatInvite(['hash' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg']);
+    } catch (\danog\MadelineProto\RPCErrorException $e) {
+        $MadelineProto->logger($e);
+    }
+
+    // $MadelineProto->messages->sendMessage(['peer' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg', 'message' => 'Testing MadelineProto!']);
+}
+$MadelineProto->echo('OK, done!');
